96ad278eff70aa48c612f4d58c9ca751f2bc746e
anasscodes <anassmtn7@gmail.com>
2024-09-12 03:08:27 +0200

    this is new just modification

--- main.js
+++ main.js
@@ -2,3 +2,25 @@ let header = document.querySelector('header');
 window.addEventListener('scroll', function(){
     header.classList.toggle('sticky' , this.window.scrollY > 0)
 })
+
+// ########################
+let bt = document.getElementById('bt');
+
+window.onscroll = function(){
+    if(screenY >= 400)
+    {
+        bt.style.display = 'inline-block';
+        
+    }else{
+        bt.style.display = 'block';
+    
+    }
+}
+
+bt.onclick = function(){
+    scroll({
+        left:0,
+        top:0,
+        behavior:"smooth"
+    })
+}
